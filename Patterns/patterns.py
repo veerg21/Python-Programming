40308a99959932f1ed06a28b122ea89111c9ec2c
veerg21 <veergupta275@gmail.com>
2022-01-14 15:20:02 +0530

    adding files

--- Patterns/patterns.py
+++ Patterns/patterns.py
@@ -5,15 +5,16 @@
 
 
 
-# for y in range(4):
-#     for i in range(y+1):
-#         print("* ", end="")
-#     print("\r")
-
+for y in range(4):
+      for i in range(y+1):
+          print("* ", end="")
+      print("\r")
 
 z=4
 for y in range(4):
     for i in range(z):
-        print("* ", end="")
+        print(" ", end="")
+    for i in range(y+1):
+        print("*", end="")
     print("\r")
     z=z-1
